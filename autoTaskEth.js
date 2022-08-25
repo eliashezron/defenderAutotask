1d77d386d675d32336c3a09bbaaba52641a77298
eliashezron <eliashezron23@gmail.com>
2022-08-25 07:54:15 +0000

    edit

--- autoTaskEth.js
+++ autoTaskEth.js
@@ -1,26 +1,26 @@
 const ABI = [
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "_withdrawerAddress",
-        "type": "address"
+        internalType: "address",
+        name: "_withdrawerAddress",
+        type: "address",
       },
       {
-        "internalType": "uint256",
-        "name": "_amount",
-        "type": "uint256"
-      }
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
     ],
-    "name": "withdrawEth",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
+    name: "withdrawEth",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
   },
 ]
 const CONTRACT = "0x068726825cadC89558D8f9173b4782E6A170cEcd"
-const { DefenderRelayProvider } = require('defender-relay-client/lib/web3');
-const Web3 = require('web3');
+const { DefenderRelayProvider } = require("defender-relay-client/lib/web3")
+const Web3 = require("web3")
 
 /**
  * @param {string} recipient  recipient address
@@ -30,19 +30,23 @@ const Web3 = require('web3');
  */
 
 async function main(recipient, amount, provider) {
-  const web3 = new Web3(provider);
-  const [from] = await web3.eth.getAccounts();
-  const contract = new web3.eth.Contract(ABI, CONTRACT, { from });
-  await contract.methods.withdrawEth(recipient, amount).send()
-  .on('receipt', function(receipt){
-    // receipt example
-    console.log(`withdrawed ${amount} to recipient ${recipient} with transaction hash ${receipt.transactionHash} and status ${receipt.status}`);
-  })  
+  const web3 = new Web3(provider)
+  const [from] = await web3.eth.getAccounts()
+  const contract = new web3.eth.Contract(ABI, CONTRACT, { from })
+  await contract.methods
+    .withdrawEth(recipient, amount)
+    .send()
+    .on("receipt", function (receipt) {
+      // receipt example
+      console.log(
+        `withdrawed ${amount} to recipient ${recipient} with transaction hash ${receipt.transactionHash} and status ${receipt.status}`
+      )
+    })
 }
 // entry point for autotask
 
 exports.handler = async function (event) {
-  const provider = new DefenderRelayProvider(event, { speed: 'fast' });
+  const provider = new DefenderRelayProvider(event, { speed: "fast" })
   // Use web3 instance for querying or sending txs, for example...
   const recipient = event.request.body.recipient
   const amount = event.request.body.amount
--- indexeth.js
+++ indexeth.js
@@ -1,15 +1,19 @@
-  const { main } = require("./autoTaskEth")
-  const { DefenderRelayProvider } = require('defender-relay-client/lib/web3');
-  const Web3 = require('web3');
-  const apiKey = "HK5c8sJGodJ5sW5MWgQqjsjyw3cJX2X5"
-  const apiSecret = "3hcQxQbzMNftKWoFFRzsU815xor6NmziFjEYhWRgff9CqEG7hEjut2k7Rug5Ue4D"
-  const creds = { apiKey, apiSecret }
-  const provider = new DefenderRelayProvider(creds,{ speed: 'fastest',gasLimit: 100000})
-  const recipient = "0xdB01d94217308046a792D864b16A35837aa52B86"
-  const amount = Web3.utils.toWei("0.001", "ether")
-  main(recipient, amount, provider)
+const { main } = require("./autoTaskEth")
+const { DefenderRelayProvider } = require("defender-relay-client/lib/web3")
+const Web3 = require("web3")
+const apiKey = "HK5c8sJGodJ5sW5MWgQqjsjyw3cJX2X5"
+const apiSecret =
+  "3hcQxQbzMNftKWoFFRzsU815xor6NmziFjEYhWRgff9CqEG7hEjut2k7Rug5Ue4D"
+const creds = { apiKey, apiSecret }
+const provider = new DefenderRelayProvider(creds, {
+  speed: "fastest",
+  gasLimit: 100000,
+})
+const recipient = "0xdB01d94217308046a792D864b16A35837aa52B86"
+const amount = Web3.utils.toWei("0.0001", "ether")
+main(recipient, amount, provider)
   .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error)
-      process.exit(1)
-    })
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
