0d63b0c16236cc8f613c76b3444a95a809929330
eliashezron <eliashezron23@gmail.com>
2022-10-04 16:11:42 +0000

    commit

--- recovery.js
+++ recovery.js
@@ -12,23 +12,39 @@ const abi = [
     ],
   },
 ]
-const addr = "0xdB01d94217308046a792D864b16A35837aa52B86"
+const account = new web3.eth.accounts()[0]
 const tokenContract = "0x1cC7b9af52fE41E0551c530A4453eF3739F22639"
 
-async function test() {
-  if (typeof Web3 !== "undefined") {
-    const web3 = new Web3(currentProvider)
-    await window.ethereum.enable()
+const privatekey = []
 
-    const contract = new web3.eth.Contract(abi, tokenContract)
-    const account = new web3.eth.accounts()[0]
-    try {
-      await contract.methods.destroy(addr).send({
-        from: account.address,
-      })
-    } catch (err) {
-      console.log("error here", err)
-    }
-  }
+const contract = new web3.eth.Contract(abi, tokenContract)
+
+const nounce = web3.eth.getTransactionCount(account)
+// transaction objec t sent with the transaction
+const tx = {
+  chainId: 4,
+  from: account,
+  gas: 100000,
+  nounce: nounce,
+  gasPrice: 200000,
+  nonce: nounce,
+  data: contract.methods.destroy(account).encodeABI(),
 }
-test()
+// sign the tranaction with the Private key to make a state change
+async function sendTransaction() {
+  const signature = await web3.eth.accounts.signTransaction(tx, privatekey)
+  // send the signed transaction to the blockchain
+  await web3.eth
+    .sendSignedTransaction(signature.rawTransaction)
+    .on("receipt", () => {
+      console.log(
+        `transaction completed with ${receipt.transactionHash} and status ${receipt.status}`
+      )
+    })
+    .on("confirmation", function (confirmationNumber, receipt) {
+      console.log(confirmationNumber, receipt)
+    })
+    .on("error", console.error)
+}
+
+sendTransaction()
